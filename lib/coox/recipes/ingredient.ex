97e1f6078fda1491aff32107d589cc75e54b7bb4
George <george@arrowsmith.codes>
2024-10-19 11:19:57 +0100

    starter app

---
+++ lib/coox/recipes/ingredient.ex
@@ -0,0 +1,22 @@
+defmodule Coox.Recipes.Ingredient do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Coox.Recipes.Recipe
+
+  schema "ingredients" do
+    field :name, :string
+    field :order, :integer, default: 0
+
+    belongs_to :recipe, Recipe
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(ingredient, attrs) do
+    ingredient
+    |> cast(attrs, [:name, :order])
+    |> validate_required([:name])
+  end
+end
