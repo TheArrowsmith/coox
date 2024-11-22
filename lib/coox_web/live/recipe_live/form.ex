9963c55e12e674a3390d5e749e8e2da123819988
George <george@arrowsmith.codes>
2024-11-22 14:17:25 +0000

    add image uploads

--- lib/coox_web/live/recipe_live/form.ex
+++ lib/coox_web/live/recipe_live/form.ex
@@ -9,6 +9,23 @@ defmodule CooxWeb.RecipeLive.Form do
     <.header><%= @page_title %></.header>
 
     <.simple_form for={@form} id="recipe-form" phx-change="validate" phx-submit="save">
+      <div>
+        <.label>Image</.label>
+
+        <%= for entry <- @uploads.image.entries do %>
+          <figure class="flex justify-around">
+            <div>
+              <.live_img_preview entry={entry} class="rounded max-h-64 shadow" />
+              <figcaption class="text-center text-sm text-gray-700 mt-2">
+                <%= entry.client_name %>
+              </figcaption>
+            </div>
+          </figure>
+        <% end %>
+
+        <.live_file_input upload={@uploads.image} class="mt-2" />
+      </div>
+
       <.input field={@form[:name]} type="text" label="Name" phx-debounce />
       <.input
         field={@form[:description]}
@@ -32,6 +49,11 @@ defmodule CooxWeb.RecipeLive.Form do
   def mount(params, _session, socket) do
     {:ok,
      socket
+     |> allow_upload(:image,
+       accept: ~w(.png .jpg),
+       max_entries: 1,
+       max_file_size: 2 * 1024 * 1024
+     )
      |> apply_action(socket.assigns.live_action, params)}
   end
 
@@ -53,9 +75,24 @@ defmodule CooxWeb.RecipeLive.Form do
     |> assign(:form, to_form(Recipes.change_recipe(recipe)))
   end
 
+  @uploads_dir Path.join([:code.priv_dir(:coox), "static", "uploads"])
+
   def handle_event("save", %{"recipe" => recipe_params}, socket) do
     case save_recipe(socket, socket.assigns.live_action, recipe_params) do
       {:ok, recipe} ->
+        image_path =
+          socket
+          |> consume_uploaded_entries(:image, fn %{path: path}, _entry ->
+            dest = Path.join(@uploads_dir, Path.basename(path))
+            File.cp!(path, dest)
+            {:ok, Path.basename(dest)}
+          end)
+          |> List.first()
+
+        if image_path do
+          Recipes.update_recipe_image_path!(recipe, image_path, socket.assigns.current_user)
+        end
+
         flash_msg =
           case socket.assigns.live_action do
             :new -> "Recipe created successfully"
