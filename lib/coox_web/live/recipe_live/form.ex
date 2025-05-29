d282de0f9ba0bb108f98430f44b95bb6c7296c69
George <george@arrowsmith.codes>
2025-05-29 12:06:31 +0100

    add 'add more' button for instructions

--- lib/coox_web/live/recipe_live/form.ex
+++ lib/coox_web/live/recipe_live/form.ex
@@ -92,6 +92,16 @@ defmodule CooxWeb.RecipeLive.Form do
                 </div>
               </div>
             </.inputs_for>
+
+            <button
+              class="mt-4 text-zinc-700"
+              name="recipe[instructions_sort][]"
+              phx-click={JS.dispatch("change")}
+              type="button"
+              value="new"
+            >
+              <.icon name="hero-plus-circle" class="h-5 w-5 relative top-[-1px]" /> add more
+            </button>
           </div>
         </div>
 
